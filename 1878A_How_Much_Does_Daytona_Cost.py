0e9f05ee996cfadcf3b6938186fbe65aa3a97a2f
KavyanshSharma09 <kavyanshsharma2004@gmail.com>
2026-09-15 14:19:46 +0530

    New Problems

---
+++ 1878A_How_Much_Does_Daytona_Cost.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n):
+    x,k = map(int,input().split())
+    arr= list(map(int,input().split()))
+    if k in arr:
+        print("YES")
+    else:
+        print("NO")
+    
---
+++ 581A_Vasya_the_Hipster.py
@@ -0,0 +1,5 @@
+a,b = map(int,input().split())
+t = min(a,b)
+t1 = max(a,b)-t
+print(t,t1//2)
+
