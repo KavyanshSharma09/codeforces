ab0e15bafae8d25e0259c556a181646e844eacb2
KavyanshSharma09 <kavyanshsharma2004@gmail.com>
2026-09-25 22:44:50 +0530

    Fix

--- 2009A_Minimize.py
+++ 2009A_Minimize.py
@@ -1,8 +1,11 @@
 n = int(input())
 for _ in range(n):
     a,b = map(int,input().split())
-    finn = float('inf')
-    for i in range(a,b):
-        temp = (i-a)+(b-i)
-        finn = min(finn, temp)
-    print(finn)
+    if a ==b:
+        print(0)
+    else:
+        finn = float('inf')
+        for i in range(a,b):
+            temp = (i-a)+(b-i)
+            finn = min(finn, temp)
+        print(finn)
